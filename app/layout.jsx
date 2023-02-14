fb95777f4d3a625d684443420b43319edce24944
Chris Crow <chriscrow@Chriss-Air.attlocal.net>
2023-02-14 13:14:34 -0600

    add Nav element

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import "./globals.css";
+import Nav from "./auth/Nav";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <Nav />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
