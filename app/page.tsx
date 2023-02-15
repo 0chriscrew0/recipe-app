557e79f66ffb4c8f4e646e550a8039edf7e5a056
Christopher Crow <0chriscrew0@gmail.com>
2023-02-15 15:20:51 -0600

    add list of Recipes to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,36 @@
 "use client";
 
+import axios from "axios";
 import AddRecipe from "./components/AddRecipe";
+import { useQuery } from "@tanstack/react-query";
+import Recipe from "./components/Recipe";
+
+// Get all recipes
+const allRecipes = async () => {
+  const response = await axios.get("/api/recipes/getRecipes");
+  return response.data;
+};
 
 export default function Home() {
+  const { data, error, isLoading } = useQuery({
+    queryFn: allRecipes,
+    queryKey: ["recipes"],
+  });
+
+  if (error) return error;
+  if (isLoading) return "Loading...";
   return (
     <main>
       <AddRecipe />
+      {data?.map((recipe) => (
+        <Recipe
+          key={recipe.id}
+          id={recipe.id}
+          name={recipe.user.name}
+          avatar={recipe.user.image}
+          title={recipe.title}
+        />
+      ))}
     </main>
   );
 }
---
+++ pages/api/recipes/getRecipes.tsx
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    // Get all recipes
+    try {
+      const data = await prisma.recipe.findMany({
+        include: {
+          user: true,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+      res.status(200).json(data);
+    } catch (err) {
+      res.status(403).json({ err: "Error has occured while fetching recipes" });
+    }
+  }
+}
