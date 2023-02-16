4031183067eb7e48525253fdd2f9a8221a5d4e93
Christopher Crow <0chriscrew0@gmail.com>
2023-02-16 12:34:31 -0600

    add functionality to add comments to recipe

--- app/components/AddComment.tsx
+++ app/components/AddComment.tsx
@@ -1,15 +1,52 @@
 "use client";
 
 import { useState } from "react";
-import { useMutation, usQueryClient } from "@tanstack/react-query";
-import axios from "axios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios, { AxiosError } from "axios";
 import { toast } from "react-hot-toast";
 
-export default function AddComment({ id }) {
+type RecipeProps = {
+  id?: string;
+};
+
+type Comment = {
+  id?: string;
+  title: string;
+};
+
+export default function AddComment({ id }: RecipeProps) {
   const [title, setTitle] = useState("");
   const [isDisabled, setIsDisabled] = useState(false);
+  const queryClient = useQueryClient();
+  let commentToastId: string;
+
+  const { mutate } = useMutation(
+    async (data: Comment) => axios.post("/api/recipes/addComment", { data }),
+    {
+      onSuccess: (data) => {
+        setTitle("");
+        setIsDisabled(false);
+        toast.success("Comment added", { id: commentToastId });
+      },
+      onError: (error) => {
+        setIsDisabled(false);
+        if (error instanceof AxiosError) {
+          toast.error("Error adding comment", { id: commentToastId });
+        }
+      },
+    }
+  );
+
+  const submitComment = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsDisabled(true);
+    commentToastId = toast.loading("Adding comment...", { id: commentToastId });
+    console.log(title, id);
+    mutate({ title, id });
+  };
+
   return (
-    <form className='my-8'>
+    <form onSubmit={submitComment} className='my-8'>
       <h3>Add comment</h3>
       <div className='flex flex-col my-2'>
         <input
@@ -22,6 +59,7 @@ export default function AddComment({ id }) {
       </div>
       <div className='flex items-center gap-2'>
         <button
+          type='submit'
           disabled={isDisabled}
           className='text-sm bg-teal-600 text-white py-4 px-2'
         >
---
+++ pages/api/recipes/addComment.ts
@@ -0,0 +1,40 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]";
+import prisma from "../../../prisma/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const session = await getServerSession(req, res, authOptions);
+    if (!session) return res.status(401).json({ message: "Please sign in" });
+
+    // Get user
+    const prismaUser = await prisma.user.findUnique({
+      where: { email: session?.user?.email },
+    });
+
+    // Add a comment
+    try {
+      const { title, id } = req.body.data;
+      if (!title.length) {
+        return res.status(401).json({ message: "Please enter a comment" });
+      }
+      const result = await prisma.comment.create({
+        data: {
+          message: title,
+          userId: prismaUser?.id,
+          recipeId: id,
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      res
+        .status(403)
+        .json({ err: "Error has occured while trying to create comment" });
+    }
+  }
+}
